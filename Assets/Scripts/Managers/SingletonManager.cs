e57415be7cf73cd5e0e0dd02e081081bdc32b109
OzanVolkan <ozan.atci.95@gmail.com>
2024-11-18 23:20:40 +0300

    Mono Singleton Manager

---
+++ Assets/Scripts/Managers/SingletonManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class SingletonManager<T> : MonoBehaviour where T: SingletonManager<T>
+    {
+        private static volatile T instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType(typeof(T)) as T;
+                }
+
+                return instance;
+            }
+        }
+    }
+}
