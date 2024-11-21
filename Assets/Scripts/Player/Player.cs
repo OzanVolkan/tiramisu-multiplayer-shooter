0b447679112e22962475a012666cc80c8b094f68
OzanVolkan <ozan.atci.95@gmail.com>
2024-11-21 03:08:16 +0300

    Player canvas values with RPC

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,6 +7,8 @@ namespace Player
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private PhotonView _photonView;
+
         #region PlayerMaterialValues
 
         [SerializeField] private Renderer _renderer;
@@ -14,13 +16,26 @@ namespace Player
         [SerializeField] private Material _redMat;
 
         #endregion
-        
+
+        #region PlayerCanvasValues
+
+        [SerializeField] private Transform _playerCanvasTrans;
+        private readonly Vector3 _canvasRotation = new Vector3(90f, 0f, 0f);
+        private readonly float _canvasZ = 0.75f;
+
+        #endregion
+
         private readonly int _maxHealth = 10;
         private int _health;
 
         private void Start()
         {
             _health = _maxHealth;
+
+            if (!_photonView.IsMine)
+                return;
+
+            _photonView.RPC(nameof(SetPlayerCanvasTransformValues), RpcTarget.AllBuffered);
         }
 
         private void OnEnable()
@@ -36,7 +51,7 @@ namespace Player
         private void TakeDamage(int damage)
         {
             _health = Mathf.Clamp(_health - damage, 0, _maxHealth);
-            
+
             if (_health <= 0)
             {
                 HandleDeath();
@@ -63,6 +78,14 @@ namespace Player
             }
 
             _renderer.materials = mats;
+        }
+
+        [PunRPC]
+        private void SetPlayerCanvasTransformValues()
+        {
+            _playerCanvasTrans.rotation = Quaternion.Euler(_canvasRotation);
+            _playerCanvasTrans.position =
+                new Vector3(_playerCanvasTrans.position.x, _playerCanvasTrans.position.y, _canvasZ);
         }
     }
 }
